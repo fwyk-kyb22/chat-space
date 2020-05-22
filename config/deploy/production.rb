677b68057df695901a663b6be22ee8f8ffd9cf17
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-05-22 21:06:50 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'http://13.113.246.184/', user: 'ec2-user', roles: %w{app db web}
+server '13.113.246.184', user: 'ec2-user', roles: %w{app db web}
